14ad474d9885f82566217d1a1bd81655a6737c98
Jay Ryu <JayRyu@CSEs-MacBook-Pro.local>
2016-10-02 20:29:38 -0700

    Fixed

---
+++ Roll a Ball/Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+
+	private Rigidbody rb;
+	public float speed;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
